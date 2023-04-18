4ef7998fef233502a32a395f5a44c9baad1d8406
obmutescences <913108060@qq.com>
2023-04-18 10:45:56 +0800

    up

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -1,79 +1,31 @@
--- https://github.com/kyazdani42/nvim-tree.lua
---
--- <CR> or o on the root folder will cd in the above directory
--- <C-]> will cd in the directory under the cursor
--- <BS> will close current opened directory or parent
--- type a to add a file. Adding a directory requires leaving a leading / at the end of the path.
--- you can add multiple directories by doing foo/bar/baz/f and it will add foo bar and baz directories and f as a file
---
--- type r to rename a file
--- type <C-r> to rename a file and omit the filename on input
--- type x to add/remove file/directory to cut clipboard
--- type c to add/remove file/directory to copy clipboard
--- type y will copy name to system clipboard
--- type Y will copy relative path to system clipboard
--- type gy will copy absolute path to system clipboard
--- type p to paste from clipboard. Cut clipboard has precedence over copy (will prompt for confirmation)
--- type d to delete a file (will prompt for confirmation)
--- type D to trash a file (configured in setup())
--- type ]c to go to next git item
--- type [c to go to prev git item
--- type - to navigate up to the parent directory of the current file/directory
--- type s to open a file with default system application or a folder with default file manager (if you want to change the command used to do it see :h nvim-tree.setup under system_open)
--- if the file is a directory, <CR> will open the directory otherwise it will open the file in the buffer near the tree
--- if the file is a symlink, <CR> will follow the symlink (if the target is a file)
--- <C-v> will open the file in a vertical split
--- <C-x> will open the file in a horizontal split
--- <C-t> will open the file in a new tab
--- <Tab> will open the file as a preview (keeps the cursor in the tree)
--- I will toggle visibility of hidden folders / files
--- H will toggle visibility of dotfiles (files/folders starting with a .)
--- R will refresh the tree
--- Double left click acts like <CR>
--- Double right click acts like <C-]>
--- W will collapse the whole tree
--- S will prompt the user to enter a path and then expands the tree to match the path
--- . will enter vim command mode with the file the cursor is on
--- C-k will toggle a popup with file infos about the file under the cursor
---
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
--- vim.g.nvim_tree_icons = {
---   default = "",
---   symlink = "",
---   git = {
---     unstaged = "",
---     staged = "S",
---     unmerged = "",
---     renamed = "➜",
---     deleted = "",
---     untracked = "U",
---     ignored = "◌",
---   },
---   folder = {
---     default = "",
---     open = "",
---     empty = "",
---     empty_open = "",
---     symlink = "",
---   },
--- }
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
 	vim.notify("nvim-tree not found!")
 	return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  --
+  -- BEGIN_DEFAULT_ON_ATTACH
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical,                opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal,              opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<CR>',  api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', '<Tab>', api.node.open.edit,					  opts('Open'))
+  vim.keymap.set('n', 'o',     api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', 'tt',     api.tree.toggle,                    opts('Toggle'))
+
 end
 
--- local gheight = vim.api.nvim_list_uis()[1].height
-local gwidth = vim.api.nvim_list_uis()[1].width
-
 nvim_tree.setup {
+	on_attach = on_attach,
 	disable_netrw = true,
 	hijack_netrw = true,
 	open_on_tab = true,
@@ -103,32 +55,11 @@ nvim_tree.setup {
 		-- height = 30,
 		hide_root_folder = false,
 		side = "right",
-		-- auto_resize = true,
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "I", "<CR>", "o" }, action = "edit" },
-				{ key = "h", action = "close_node" },
-				{ key = "v", action = "vsplit" },
-				{ key = "O", action = "cd" },
-				{ key = "tt", action = "toggle" },
-				{ key = "<Tab>", action = "edit" }
-			},
-		},
 		number = false,
 		relativenumber = false,
 		float = {
 			enable = false,
 			quit_on_focus_loss = false,
-			-- open_win_config = {
-			-- 	-- relative = "editor",
-			-- 	relative = "win",
-			-- 	border = "none",
-			-- 	width = 35,
-			-- 	height = 35,
-			-- 	row = 0,
-			-- 	col = gwidth -15,
-   --        },
 		}
 	},
 	filters = {
@@ -144,11 +75,6 @@ nvim_tree.setup {
 		}
 	},
 }
-
--- with relative path
--- require "nvim-tree.events".on_file_created(function(file) vim.cmd("edit " .. file.fname) end)
--- with absolute path
--- require"nvim-tree.events".on_file_created(function(file) vim.cmd("edit "..vim.fn.fnamemodify(file.fname, ":p")) end)
 
 -- auto close feature
 vim.cmd(
