b849ba6c53fbc84342d060aae0b35a2e425fa5ae
zerone <zerone@zeronedeiMac.local>
2023-09-25 14:02:36 +0800

    update

--- lua/conf/go.lua
+++ lua/conf/go.lua
@@ -37,11 +37,13 @@ require('go').setup({
 	--    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>F", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap=true, silent=true})
 	-- end
 	-- to setup a table of codelens
-	lsp_diag_hdlr = true, -- hook lsp diag handler
-	lsp_diag_underline = true,
+	diagnostic = {
+		hdlr = true,
+		underline = true,
+		virtual_text = { space = 0, prefix = "" },
+		signs = false,
+	}, -- hook lsp diag handler
 	-- virtual text setup
-	lsp_diag_virtual_text = { space = 0, prefix = "" },
-	lsp_diag_signs = false,
 	lsp_diag_update_in_insert = false,
 	lsp_document_formatting = true,
 	-- set to true: use gopls to format
