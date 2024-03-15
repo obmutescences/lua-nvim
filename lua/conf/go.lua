3a9638e90cd263eee0e95e68354bc64d5f468559
obmutescences <913108060@qq.com>
2024-03-15 15:39:23 +0800

    upadte

--- lua/conf/go.lua
+++ lua/conf/go.lua
@@ -1,11 +1,11 @@
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		require('go.format').goimport()
-	end,
-	group = format_sync_grp,
-})
+-- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*.go",
+-- 	callback = function()
+-- 		require('go.format').goimport()
+-- 	end,
+-- 	group = format_sync_grp,
+-- })
 require("go").setup({
 	disable_defaults = false, -- true|false when true set false to all boolean settings and replace all table
 	-- settings with {}
