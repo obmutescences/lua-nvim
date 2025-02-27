1a79c9df93cde43468e4be51d7c35fa216dad675
zerone <913108060@qq.com>
2025-02-27 18:17:53 +0800

    update: add deepseek for avante

--- lua/conf/avante.lua
+++ lua/conf/avante.lua
@@ -1,6 +1,6 @@
 require("avante").setup({
 	---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-	provider = "openai", -- Recommend using Claude
+	provider = "deepseek", -- Recommend using Claude
 	auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
 	claude = {
 		endpoint = "https://api.anthropic.com",
@@ -14,6 +14,14 @@ require("avante").setup({
 		timeout = 30000, -- Timeout in milliseconds
 		temperature = 0,
 		max_tokens = 4096,
+	},
+	vendors = {
+		deepseek = {
+			__inherited_from = "openai",
+			api_key_name = "DEEPSEEK_API_KEY",
+			endpoint = "https://api.deepseek.com",
+			model = "deepseek-chat",
+		},
 	},
 	-- openai = {
 	-- 	endpoint = "https://api.deepseek.com/v1",
