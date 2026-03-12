ce5826a74fc4f8dfbde292e146b0cb5ee9da7795
zerone <913108060@qq.com>
2026-03-12 14:07:32 +0800

    :wrench:  update: neovide config

--- lua/options.lua
+++ lua/options.lua
@@ -67,12 +67,12 @@ vim.g.neovide_cursor_vfx_particle_density = 20
 vim.g.neovide_cursor_vfx_particle_speed = 50
 vim.g.neovide_cursor_animation_length = 0.2
 vim.g.neovide_cursor_short_animation_length = 0.1
-vim.g.neovide_cursor_trail_size = 0.4
+vim.g.neovide_cursor_trail_size = 0.35
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_scroll_animation_length = 0.25
+vim.g.neovide_scroll_animation_length = 0.2
 
 vim.g.neovide_floating_blur = 1
 vim.g.neovide_floating_opacity = 1
