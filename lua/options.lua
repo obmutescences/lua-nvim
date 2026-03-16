7341f94c6694a6e83a8413feab5c4f53524e4d92
zerone <913108060@qq.com>
2026-03-16 10:54:19 +0800

    :wrench:  update: neovide config

--- lua/options.lua
+++ lua/options.lua
@@ -49,14 +49,14 @@ vim.opt.fillchars = {
 	horizdown = " ", -- 下降的水平分割线
 	eob = " ", -- 空白处的 '~' 符号
 }
-vim.o.guicursor =
-	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+-- vim.o.guicursor =
+-- 	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 
 -- -- neovide
 vim.g.neovide_refresh_rate = 180
--- vim.g.neovide_refresh_rate_idle = 180
+vim.g.neovide_refresh_rate_idle = 180
 vim.g.neovide_no_idle = true
 vim.g.neovide_cursor_vfx_mode = { "pixiedust", "sonicboom" }
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
@@ -65,7 +65,7 @@ vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
 vim.g.neovide_cursor_vfx_particle_highlight_lifetime = 1.0
 vim.g.neovide_cursor_vfx_particle_density = 20
 vim.g.neovide_cursor_vfx_particle_speed = 50
-vim.g.neovide_cursor_animation_length = 0.2
+vim.g.neovide_cursor_animation_length = 0.25
 vim.g.neovide_cursor_short_animation_length = 0.1
 vim.g.neovide_cursor_trail_size = 0.35
 vim.g.neovide_cursor_antialiasing = true
@@ -102,6 +102,7 @@ vim.g.neovide_progress_bar_enabled = true
 vim.g.neovide_progress_bar_height = 5.0
 vim.g.neovide_progress_bar_animation_speed = 200.0
 vim.g.neovide_progress_bar_hide_delay = 0.2
+vim.g.neovide_cursor_cell_color_fallback = true
 -- vim.g.neovide_remember_window_size          = true
 local alpha = function()
 	return string.format("%x", math.floor(255 * 0.8))
