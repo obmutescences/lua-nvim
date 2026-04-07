7e1d8cd3e2baee5446e07fce1f9cef8baab3786c
zerone <913108060@qq.com>
2026-04-07 18:12:06 +0800

    :sparkles:  feat: add tree sitter manager plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -187,6 +187,18 @@ require("lazy").setup({
 		build = ":TSUpdate",
 	},
 
+	{
+		"romus204/tree-sitter-manager.nvim",
+		dependencies = {}, -- tree-sitter CLI must be installed system-wide
+		config = function()
+			require("tree-sitter-manager").setup({
+				-- Optional: custom paths
+				-- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+				-- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+			})
+		end,
+	},
+
 	-- start bord
 	{
 		"startup-nvim/startup.nvim",
