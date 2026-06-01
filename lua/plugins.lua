9e930b871e34e0e53ccc17b1af8a93bd309a03e0
zerone <913108060@qq.com>
2026-06-01 11:55:51 +0800

    ✨ feat: add render-markdown.nvim plugin with tree-sitter deps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -479,4 +479,13 @@ require("lazy").setup({
 
 	-- colorful cmp menu
 	"xzbdmw/colorful-menu.nvim",
+
+	-- markdown
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		opts = {},
+	},
 })
